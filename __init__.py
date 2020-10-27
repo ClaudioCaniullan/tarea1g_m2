3810b303e08eac854bb04df46d0d14f56a001ccd
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-27 16:21:50 -0300

    archivos fisica init y funciones no contenian los archivos pedidos

--- __init__.py
+++ __init__.py
@@ -1,38 +1,20 @@
-<<<<<<< HEAD
 
-# init funciones
+# init funciones matematicas
 
 from .nuevos_calculos import area_circulo, perimetro_circulo
 from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
 from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
-
+# init funciones economia
 from .nuevos_calculos import tasa_actividad
 from .nuevos_calculos import tasa_ocupacion
 from .nuevos_calculos import tasa_paro
 from .nuevos_calculos import rentabilidad_economica
 
-<<<<<<< HEAD
-=======
-
-
-
-		
-
-=======
-
-# init funciones
+# init funciones fisica
 from .nuevos_calculos import area_circulo, perimetro_circulo
 from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
 from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
 
 
-
-
-
-		
-
->>>>>>> dba7581893103e093929dd9371d25456b61bce69
-		
->>>>>>> fisica

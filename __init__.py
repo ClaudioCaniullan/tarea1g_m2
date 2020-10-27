0aed0bd426922d1e4fda3b29b8627b8f5d377372
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-27 15:54:49 -0300

    agregue init y nuevos_calculos

---
+++ __init__.py
@@ -0,0 +1,14 @@
+
+# init funciones
+from .nuevos_calculos import area_circulo, perimetro_circulo
+from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
+from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
+
+
+
+
+
+
+		
+
+		
---
+++ nuevos_calculos.py
@@ -0,0 +1,39 @@
+import math
+from math import pi 
+
+#definicion de las funciones matematicas
+
+def area_circulo(r):
+	area = pi*r**2
+	return print('el area del ciruclo es: ' + str(area))
+
+def perimetro_circulo(r):
+	perimetro = 2*pi*r
+	return print('el perimetro del circulo es: ' + str(perimetro))
+
+
+
+def area_triangulo_rec(b,h):
+	area = (b*h)/2
+	return print('El area del triangulo rectangulo es:'+ str(area))
+
+def perimetro_triangulo_rec(b,h):
+	perimetro = b + h + (b**2 + h**2)**0.5
+	return print('El perimetro del triangulo rectangulo es:'+ str(perimetro))
+
+
+
+def area_rectangulo(b,h):
+	area = (b*h)
+	return print('El area del rectangulo es: '+ str(area))
+
+def perimetro_rectangulo(b,h):
+	perimetro = 2*(b + h)
+	return print('El perimetro del rectangulo es: '+ str(perimetro))
+
+
+def distancia_recorrida(t,v):
+	distancia = t*v
+	return print('La distancia recorrida es: '+ str(distancia))
+
+

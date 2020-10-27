24e8c1eebbf835048dc18df0803f57c8e8159858
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-27 16:19:30 -0300

    corregi conflicto linas head sin proposito borradas

--- __init__.py
+++ __init__.py
@@ -1,24 +1,13 @@
 
-<<<<<<< HEAD
 # init funciones
+
 from .nuevos_calculos import area_circulo, perimetro_circulo
 from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
 from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
-=======
->>>>>>> e2858318d5ead4d8ba1012e43105efae42f6e71a
 
 
-
-
-
-<<<<<<< HEAD
-
-		
-
-		
-=======
 from .nuevos_calculos import tasa_actividad
 from .nuevos_calculos import tasa_ocupacion
 from .nuevos_calculos import tasa_paro
 from .nuevos_calculos import rentabilidad_economica
->>>>>>> e2858318d5ead4d8ba1012e43105efae42f6e71a
+

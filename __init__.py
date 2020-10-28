09e54db1555da2df5d50e7d50bb41d83fb066463
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-28 11:46:24 -0300

    se corrijen conflictos aparicion HEAD

--- __init__.py
+++ __init__.py
@@ -1,53 +1,19 @@
 
-<<<<<<< HEAD
-# init funciones matematicas
+# init funciones matematicas
 
-from .nuevos_calculos import area_circulo, perimetro_circulo
-from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
-from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
-
-# init funciones economia
-from .nuevos_calculos import tasa_actividad
-from .nuevos_calculos import tasa_ocupacion
-from .nuevos_calculos import tasa_paro
-from .nuevos_calculos import rentabilidad_economica
-
-
-
-
-=======
-
-# init funciones
 from .nuevos_calculos import area_circulo, perimetro_circulo
 from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
 from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# init funciones economia
+from .nuevos_calculos import tasa_actividad
+from .nuevos_calculos import tasa_ocupacion
+from .nuevos_calculos import tasa_paro
+from .nuevos_calculos import rentabilidad_economica
 
 
 from .nuevos_calculos import velocidad, distancia, tiempo, aceleracion
 
 
->>>>>>> fisica
+
+

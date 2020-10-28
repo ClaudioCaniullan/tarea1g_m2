dba7581893103e093929dd9371d25456b61bce69
francisca <franciscacruzgantes@gmail.com>
2020-10-27 21:22:52 -0300

    agregué programa 3

---
+++ programa3.py
@@ -0,0 +1,15 @@
+import paquete_cientifico2 as p2
+
+def calcular2():
+    resultado=p2.velocidad()
+    print("resultado" +str(resultado)) 
+    resultado1=p2.distancia()
+    print("resultado" +str(resultado1)) 
+    resultado2=p2.tiempo()
+    print("resultado" +str(resultado2)) 
+    resultado3=p2.aceleracion()
+    print("resultado" +str(resultado3)) 
+
+if __name__ == '__main__':
+    calcular2()
+    

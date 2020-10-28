dcda2006f54f5fcc23e2df41a7bd0ba4f4b0cf0a
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-28 11:18:47 -0300

    se corrigen conflicto entre lineas

--- nuevos_calculos.py
+++ nuevos_calculos.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import math
 from math import pi 
 
@@ -39,48 +38,40 @@ def distancia_recorrida(t,v):
 	return print('La distancia recorrida es: '+ str(distancia))
 
 
-=======
-import math
-from math import pi 
-
-#definicion de las funciones matematicas
-
-def area_circulo(r):
-	area = pi*r**2
-	return print('el area del ciruclo es: ' + str(area))
-
-def perimetro_circulo(r):
-	perimetro = 2*pi*r
-	return print('el perimetro del circulo es: ' + str(perimetro))
 
 
 
-def area_triangulo_rec(b,h):
-	area = (b*h)/2
-	return print('El area del triangulo rectangulo es:'+ str(area))
-
-def perimetro_triangulo_rec(b,h):
-	perimetro = b + h + (b**2 + h**2)**0.5
-	return print('El perimetro del triangulo rectangulo es:'+ str(perimetro))
 
 
 
-def area_rectangulo(b,h):
-	area = (b*h)
-	return print('El area del rectangulo es: '+ str(area))
-
-def perimetro_rectangulo(b,h):
-	perimetro = 2*(b + h)
-	return print('El perimetro del rectangulo es: '+ str(perimetro))
 
 
-def distancia_recorrida(t,v):
-	distancia = t*v
-	return print('La distancia recorrida es: '+ str(distancia))
 
 
->>>>>>> dba7581893103e093929dd9371d25456b61bce69
-=======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -185,4 +176,4 @@ def aceleracion():
     return print("la aceleración es:"+str(aceleración))
 
 
->>>>>>> e2e90ba8b79a011683dc9b0b836e238315f2b8b5
+

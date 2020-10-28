b5af990cc29798d84d474bf7e1dbd7f72a7f4bab
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-28 11:31:36 -0300

    se eliminas lineas HEAD locales

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 # init funciones
 from .nuevos_calculos import area_circulo, perimetro_circulo
@@ -10,21 +9,3 @@ from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_re
 
 
 
-		
-
-=======
-
-# init funciones
-from .nuevos_calculos import area_circulo, perimetro_circulo
-from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
-from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
-
-
-
-
-
-
-		
-
->>>>>>> dba7581893103e093929dd9371d25456b61bce69
-		

ded90103bd7d840a6adac4c66bc835db25cbdc3f
francisca <franciscacruzgantes@gmail.com>
2020-10-28 11:29:40 -0300

    modificamos init_py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,20 @@
 
-# init funciones
-from .nuevos_calculos import area_circulo, perimetro_circulo
-from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
-from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
 
 
 
 
 
-		
 
-		
+
+
+
+
+
+
+
+
+
+
+
+from .nuevos_calculos import velocidad, distancia, tiempo, aceleracion

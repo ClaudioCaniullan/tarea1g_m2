f747e56b84488e4c503c4179c534f78fe0e35dae
Maguilera85 <drianketing@gmail.com>
2020-10-27 21:14:35 -0300

    agregué mi parte del trabajo

---
+++ __init__.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+from .nuevos_calculos import tasa_actividad
+from .nuevos_calculos import tasa_ocupacion
+from .nuevos_calculos import tasa_paro
+from .nuevos_calculos import rentabilidad_economica
---
+++ nuevos_calculos.py
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def tasa_actividad(poblacion_activa, poblacion_total):
+    return (poblacion_activa / poblacion_total) * 100
+    
+
+def tasa_ocupacion(poblacion_ocupada, poblacion_activa):
+    resultado = (poblacion_ocupada / poblacion_activa) * 100
+    return resultado
+
+def tasa_paro(poblacion_parada, poblacion_activa):
+    resultado = (poblacion_parada / poblacion_activa) * 100
+    return resultado
+
+def rentabilidad_economica(bait, activo):
+    resultado = (bait / activo) * 100
+    return resultado
+
+
+
+# if __name__ == "__main__":
+    
+# tasa_actividad()
+
+# tasa_actividad()
+
+# tasa_paro()
+
+# rentabilidad_economica()

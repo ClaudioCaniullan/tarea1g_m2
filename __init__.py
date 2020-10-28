48d3e46b66d5b935a13ebfa35a0eeba79cd3e515
claudio.caniullan <claudio.caniullan@usach.cl>
2020-10-28 11:33:36 -0300

    se corrigen conflictos aparicion HEAD

--- __init__.py
+++ __init__.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-
-# init funciones
-from .nuevos_calculos import area_circulo, perimetro_circulo
-from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
-from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
 
-
-
-
-
-=======
+# init funciones
+from .nuevos_calculos import area_circulo, perimetro_circulo
+from .nuevos_calculos import area_triangulo_rec, perimetro_triangulo_rec
+from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_recorrida
 
 
 
@@ -30,5 +23,13 @@ from .nuevos_calculos import area_rectangulo, perimetro_rectangulo, distancia_re
 
 
 
-from .nuevos_calculos import velocidad, distancia, tiempo, aceleracion
->>>>>>> ded90103bd7d840a6adac4c66bc835db25cbdc3f
+
+
+
+
+
+
+
+from .nuevos_calculos import velocidad, distancia, tiempo, aceleracion
+
+
